a39f9f9a31864483f3109d8fb4022f59b03d1faf
Juan David <gottygarcia@gmail.com>
2020-12-28 00:10:28 -0500

    travis config

--- src/__test__/setupTest.js
+++ src/__test__/setupTest.js
@@ -2,3 +2,5 @@ import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter() })
+//Configuración para trabajar con peticiones http
+global.fetch = require('jest-fetch-mock')
